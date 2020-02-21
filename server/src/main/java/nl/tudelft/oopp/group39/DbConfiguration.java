a0668b49cc71cc739e6e1af75a83aa25b1fb37e9
acicimen <g.a.cicimen@student.tudelft.nl>
2020-02-21 14:43:34 +0100

    Fixed Test Errors, reverted to h2 db.

--- server/src/main/java/nl/tudelft/oopp/group39/DbConfiguration.java
+++ server/src/main/java/nl/tudelft/oopp/group39/DbConfiguration.java
@@ -13,9 +13,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories
-@PropertySource("application-dev.properties")
+@PropertySource("classpath:application-dev.properties")
 @EnableTransactionManagement
-
 public class DbConfiguration {
     @Autowired
     private Environment environment;
