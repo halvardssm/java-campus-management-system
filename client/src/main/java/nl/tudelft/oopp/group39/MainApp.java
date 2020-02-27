e018f89d83514fd74a19e5f090926929f71ab942
acicimen <g.a.cicimen@student.tudelft.nl>
2020-02-27 23:00:41 +0100

    Modified and added List View, added a button that adds cells that are placeholders

--- client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
@@ -1,9 +1,9 @@
 package nl.tudelft.oopp.group39;
 
-import nl.tudelft.oopp.group39.views.UsersDisplay;
+import nl.tudelft.oopp.group39.views.RoomListView;
 
 public class MainApp {
     public static void main(String[] args) {
-        UsersDisplay.main(new String[0]);
+        RoomListView.main(args);
     }
 }
--- client/src/main/java/nl/tudelft/oopp/group39/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/group39/communication/ServerCommunication.java
@@ -1,9 +1,12 @@
 package nl.tudelft.oopp.group39.communication;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+
 
 public class ServerCommunication {
 
@@ -16,7 +19,7 @@ public class ServerCommunication {
      */
     public static String getUsers() {
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/user")).build();
-        HttpResponse<String> response = null;
+        HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
@@ -29,4 +32,22 @@ public class ServerCommunication {
         return response.body();
     }
 
+    private static String get(String serverRequest) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(serverRequest)).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            System.out.println("Status: " + response.statusCode());
+        }
+        return response.body();
+    }
+
+    public static String getRooms() throws IOException, InterruptedException {
+        return get("http://localhost:8080/room");
+    }
+
+    public static String getBuildings() throws IOException, InterruptedException {
+        return get("http://localhost:8080/building");
+    }
 }
