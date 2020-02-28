09167eee34321dced862b80ac4d8f4e014c15b4a
Sven van den Berg <S.T.vandenBerg@student.tudelft.nl>
2020-02-28 15:46:47 +0100

    Removed some duplicate code.

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -16,48 +16,6 @@ public class ServerCommunication {
      */
     public static String getQuote() {
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/quote")).build();
-        return HttpRequest(request);
-    }
-
-    //IMPORTANT FOR SUNDAY
-    public static String getUsers() {
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/user")).build();
-        return HttpRequest(request);
-    }
-
-    public static String getBuildings() {
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/building")).build();
-        return HttpRequest(request);
-    }
-    public static String getRooms() {
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/room")).build();
-        return HttpRequest(request);
-    }
-
-    public static String getFacilities() {
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/facility")).build();
-        return HttpRequest(request);
-    }
-
-    public static String getRoomFacilities() {
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8080/roomfacility")).build();
-        return HttpRequest(request);
-    }
-
-    public static String getFilteredBuildings(String name, String location, String open, String closed, String capacity) {
-        String urlString = "http://localhost:8080/building?capacity=" + capacity + "&building=" + name + "&location=" + location + "&open=" + open + "&closed=" + closed;
-        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(urlString)).build();
-        return HttpRequest(request);
-    }
-
-    public static String addBuilding(String name, String location, String description, String open, String closed) {
-        HttpRequest.BodyPublisher newBuilding = HttpRequest.BodyPublishers.ofString("{\"name\": \"" + name + "\", \"location\":\"" + location + "\", \"description\":\"" + description + "\", \"open\":\"" + open + "\", \"closed\":\"" + closed + "\"}");
-        HttpRequest request = HttpRequest.newBuilder().POST(newBuilding).uri(URI.create("http://localhost:8080/building")).header("Content-Type", "application/json").build();
-        return HttpRequest(request);
-    }
-
-    public static String HttpRequest(HttpRequest req) {
-        HttpRequest request = req;
         HttpResponse<String> response = null;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/MainSceneController.java
@@ -1,7 +1,6 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import javafx.scene.control.Alert;
-import javafx.scene.layout.Region;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 
 public class MainSceneController {
@@ -9,23 +8,11 @@ public class MainSceneController {
     /**
      * Handles clicking the button.
      */
-    public void getUsers() {
-        buttonClicked("getUsers");
-    }
-
-    public void buttonClicked(String function) {
+    public void buttonClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-        alert.setTitle("Users shown.");
+        alert.setTitle("Quote for you");
         alert.setHeaderText(null);
-        switch(function) {
-            //IMPORTANT FOR SUNDAY
-            case "getUsers":
-                alert.setContentText(ServerCommunication.getUsers());
-                break;
-            default:
-                alert.setContentText("No such function");
-        }
+        alert.setContentText(ServerCommunication.getQuote());
         alert.showAndWait();
     }
 }
