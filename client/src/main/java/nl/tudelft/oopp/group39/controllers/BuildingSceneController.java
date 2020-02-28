d6134f010b29e9b1544f0bb39146c78c44c6c672
acicimen <g.a.cicimen@student.tudelft.nl>
2020-02-28 14:03:40 +0100

    Commit 2 - The placeholders are now actual cells that can retrieve JS objects from server, and the refresh button refreshes them.

---
+++ client/src/main/java/nl/tudelft/oopp/group39/controllers/BuildingSceneController.java
@@ -0,0 +1,95 @@
+package nl.tudelft.oopp.group39.controllers;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import netscape.javascript.JSObject;
+import nl.tudelft.oopp.group39.communication.ServerCommunication;
+
+public class BuildingSceneController {
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private GridPane newBuilding;
+
+    /**
+     * Opens login window.
+     *
+     * @throws IOException when not found.
+     */
+    public void toLogin() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
+
+        Stage login = new Stage();
+        login.setScene(new Scene(root));
+        login.show();
+    }
+
+    public void refreshBuildings() {
+        Alert alert = (new Alert(Alert.AlertType.INFORMATION));
+
+        flowPane.getChildren().clear();
+
+        try {
+            String room = ServerCommunication.getBuildings();
+
+            JsonArray buildingArray = (JsonArray) JsonParser.parseString(room);
+
+            for (JsonElement building : buildingArray) {
+                newBuilding = FXMLLoader.load(getClass().getResource("/buildingCell.fxml"));
+
+                Label name = (Label) newBuilding.lookup("#bname");
+                name.setText(((JsonObject) building).get("name").getAsString());
+
+                String bDetails = ((JsonObject) building).get("location").getAsString()
+                    + "\n" + ((JsonObject) building).get("description").getAsString()
+                    + "\n" + "Max. Capacity"
+                    + "\n" + "Opening times: " + ((JsonObject) building).get("open").getAsString()
+                    + " - " + ((JsonObject) building).get("closed").getAsString();
+
+                Label details = (Label) newBuilding.lookup("#bdetails");
+                details.setText(bDetails);
+
+                flowPane.getChildren().add(newBuilding);
+            }
+            return;
+        } catch (InterruptedException e) {
+            alert.setContentText("Error: Interrupted");
+        } catch (IOException e) {
+            alert.setContentText("Error: Wrong IO");
+        }
+        alert.showAndWait();
+    }
+
+    public void alertAllBuildings() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.setTitle("Users shown.");
+        alert.setHeaderText(null);
+        try {
+            alert.setContentText(ServerCommunication.getBuildings());
+        } catch (Exception e) {
+            alert.setContentText("Error Occurred.");
+        }
+        alert.showAndWait();
+    }
+
+//    public void removeBuilding() {
+//        flowPane.getChildren().clear();
+//    }
+
+}
