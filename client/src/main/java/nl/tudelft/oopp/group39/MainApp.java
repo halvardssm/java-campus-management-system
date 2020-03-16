28e94dd4c9b2e32e3e0b7777efe75ad33d099e3d
Alexandru Toader <A.Toader@student.tudelft.nl>
2020-03-16 03:07:22 +0100

    Admin Panel Communication Attempt- Slight FXML change

--- client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
@@ -1,9 +1,9 @@
 package nl.tudelft.oopp.group39;
 
-import nl.tudelft.oopp.group39.views.LoginPage;
+import nl.tudelft.oopp.group39.views.AdminPanel;
 
 public class MainApp {
     public static void main(String[] args) {
-        LoginPage.main(new String[0]);
+        AdminPanel.main(new String[0]);
     }
 }
---
+++ client/src/main/java/nl/tudelft/oopp/group39/views/UsersDisplay.java
@@ -0,0 +1,58 @@
+package nl.tudelft.oopp.group39.views;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.group39.controllers.BuildingSceneController;
+import nl.tudelft.oopp.group39.controllers.MainSceneController;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class UsersDisplay extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static Stage window;
+    private static Parent root;
+    private static int width = 700;
+    private static int height = 600;
+
+    @FXML
+    public static void sceneHandler(String name) throws IOException {
+        System.out.println("Scene changing...");
+        root = FXMLLoader.load(UsersDisplay.class.getResource(name));
+        window.setScene(new Scene(root, width, height));
+    }
+
+    @FXML
+    public static MainSceneController sceneControllerHandler(String name) throws IOException {
+        System.out.println("Scene changing...");
+
+        FXMLLoader loader = new FXMLLoader(UsersDisplay.class.getResource(name));
+        root = loader.load();
+        window.setScene(new Scene(root, width, height));
+
+        return loader.getController();
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("/buildingListView.fxml");
+        loader.setLocation(xmlUrl);
+        root = loader.load();
+
+        window = primaryStage;
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.setTitle("Campus Management");
+        primaryStage.show();
+    }
+}
+
