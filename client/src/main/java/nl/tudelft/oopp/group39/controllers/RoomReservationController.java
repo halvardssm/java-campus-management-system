c05d81f69a0469d7db23173402dfc937f28cd2dc
Chuckyin Sin <C.Y.Sin@student.tudelft.nl>
2020-03-19 14:21:18 +0100

    Added 24 buttons and changed some methods accordingly

--- client/src/main/java/nl/tudelft/oopp/group39/controllers/RoomReservationController.java
+++ client/src/main/java/nl/tudelft/oopp/group39/controllers/RoomReservationController.java
@@ -17,6 +17,54 @@ public class RoomReservationController extends MainSceneController {
     private Button reserveButton;
     @FXML
     private Button backButton;
+    @FXML
+    private Button timeslotButton0;
+    @FXML
+    private Button timeslotButton1;
+    @FXML
+    private Button timeslotButton2;
+    @FXML
+    private Button timeslotButton3;
+    @FXML
+    private Button timeslotButton4;
+    @FXML
+    private Button timeslotButton5;
+    @FXML
+    private Button timeslotButton6;
+    @FXML
+    private Button timeslotButton7;
+    @FXML
+    private Button timeslotButton8;
+    @FXML
+    private Button timeslotButton9;
+    @FXML
+    private Button timeslotButton10;
+    @FXML
+    private Button timeslotButton11;
+    @FXML
+    private Button timeslotButton12;
+    @FXML
+    private Button timeslotButton13;
+    @FXML
+    private Button timeslotButton14;
+    @FXML
+    private Button timeslotButton15;
+    @FXML
+    private Button timeslotButton16;
+    @FXML
+    private Button timeslotButton17;
+    @FXML
+    private Button timeslotButton18;
+    @FXML
+    private Button timeslotButton19;
+    @FXML
+    private Button timeslotButton20;
+    @FXML
+    private Button timeslotButton21;
+    @FXML
+    private Button timeslotButton22;
+    @FXML
+    private Button timeslotButton23;
 
     public static void showAlert(Alert.AlertType alertType, String title, String content) {
         Alert alert = new Alert(alertType);
@@ -29,18 +77,38 @@ public class RoomReservationController extends MainSceneController {
     @FXML
     private void reserveRoom() {
         LocalDate localDate = date.getValue();
-        if (!checkEmpty(localDate)) {
+        if (!checkEmpty(localDate, timeslotButton0, timeslotButton1, timeslotButton2, timeslotButton3,
+            timeslotButton4, timeslotButton5, timeslotButton6, timeslotButton7, timeslotButton8,
+            timeslotButton9, timeslotButton10, timeslotButton11, timeslotButton12, timeslotButton13,
+            timeslotButton14, timeslotButton15, timeslotButton16, timeslotButton17, timeslotButton18,
+            timeslotButton19, timeslotButton20, timeslotButton21, timeslotButton22, timeslotButton23)) {
             showAlert(Alert.AlertType.INFORMATION, "", "Reservation successful.");
         }
         System.out.println(localDate);
     }
 
-    public boolean checkEmpty(LocalDate localDate) {
-        if (localDate == null ||) {
+    public boolean checkEmpty(LocalDate localDate, Button button0, Button button1, Button button2, Button button3,
+                              Button button4, Button button5, Button button6, Button button7, Button button8, Button button9,
+                              Button button10, Button button11, Button button12, Button button13, Button button14, Button button15,
+                              Button button16, Button button17, Button button18, Button button19, Button button20, Button button21,
+                              Button button22, Button button23) {
+        if (localDate == null || button0 == null && button1 == null && button2 == null && button3 == null
+            && button4 == null && button5 == null && button6 == null && button7 == null && button8 == null && button9 == null
+            && button10 == null && button11 == null && button12 == null && button13 == null && button14 == null && button15 == null
+            && button16 == null && button17 == null && button18 == null && button19 == null && button20 == null && button21 == null
+            && button22 == null && button23 == null) {
             showAlert(Alert.AlertType.ERROR, "", "Please fill in all the fields.");
-            return true;
-        } else {
             return false;
+        } else {
+            return true;
         }
+    }
+
+    public void clickTimeslot() {
+
+    }
+
+    public void backToRoom() {
+
     }
 }
