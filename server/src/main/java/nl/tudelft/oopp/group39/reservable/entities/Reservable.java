907e40637615c4d24a229a94a7e6138595949521
Halvard Mørstad <h.s.s.morstad@student.tudelft.nl>
2020-03-22 21:42:51 +0100

    added abstract entity

--- server/src/main/java/nl/tudelft/oopp/group39/reservable/entities/Reservable.java
+++ server/src/main/java/nl/tudelft/oopp/group39/reservable/entities/Reservable.java
@@ -2,16 +2,11 @@ package nl.tudelft.oopp.group39.reservable.entities;
 
 import static nl.tudelft.oopp.group39.config.Utils.initSet;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
@@ -19,41 +14,26 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import nl.tudelft.oopp.group39.building.entities.Building;
+import nl.tudelft.oopp.group39.config.AbstractEntity;
 import nl.tudelft.oopp.group39.reservation.entities.ReservationAmount;
 
 @Entity
 @Table(name = Reservable.TABLE_NAME)
 @Inheritance(strategy = InheritanceType.JOINED)
-@JsonIdentityInfo(
-    generator = ObjectIdGenerators.PropertyGenerator.class,
-    property = Reservable.COL_ID
-)
 @JsonIgnoreProperties(allowSetters = true, value = {Reservable.COL_RESERVATIONS})
-public class Reservable {
+public class Reservable extends AbstractEntity {
     public static final String TABLE_NAME = "reservables";
     public static final String MAPPED_NAME = "reservable";
-    public static final String COL_ID = "id";
     public static final String COL_PRICE = "price";
     public static final String COL_BUILDING = "building";
     public static final String COL_RESERVATIONS = "reservations";
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     private Double price;
     @ManyToOne
     @JoinColumn(name = Building.MAPPED_NAME) //TODO Change to id
     private Building building;
     @OneToMany(mappedBy = MAPPED_NAME)
     private Set<ReservationAmount> reservations = new HashSet<>();
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public Building getBuilding() {
         return building;
