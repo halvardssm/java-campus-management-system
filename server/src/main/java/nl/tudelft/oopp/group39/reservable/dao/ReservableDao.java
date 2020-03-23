325a50ae722735b7f08bf554b993771c56deab56
Halvard Mørstad <h.s.s.morstad@student.tudelft.nl>
2020-03-23 14:50:42 +0100

    fixed listing filter

--- server/src/main/java/nl/tudelft/oopp/group39/reservable/dao/ReservableDao.java
+++ server/src/main/java/nl/tudelft/oopp/group39/reservable/dao/ReservableDao.java
@@ -1,5 +1,6 @@
 package nl.tudelft.oopp.group39.reservable.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.EntityManager;
@@ -7,6 +8,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import nl.tudelft.oopp.group39.building.entities.Building;
 import org.springframework.stereotype.Component;
@@ -28,13 +30,16 @@ public class ReservableDao {
         CriteriaQuery<T> q = cb.createQuery(resultClass);
         Root<T> c = q.from(resultClass);
         q.select(c);
+        List<Predicate> predicates = new ArrayList<>();
 
         if (params.containsKey(Building.MAPPED_NAME)) {
-            q.where(cb.equal(
+            predicates.add(cb.equal(
                 c.get(Building.MAPPED_NAME).get(Building.COL_ID),
                 params.get(Building.MAPPED_NAME)
             ));
         }
+
+        q.where(cb.and(predicates.toArray(new Predicate[0])));
 
         TypedQuery<T> query = em.createQuery(q);
         return query.getResultList();
