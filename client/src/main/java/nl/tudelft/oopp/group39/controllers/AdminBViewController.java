58a28dd9d611a207b991653668470ffa0e782f2b
Alexandru Toader <A.Toader@student.tudelft.nl>
2020-03-27 04:26:21 +0100

    Admin Panel - FXML Building controller and FXML + Room edits

--- client/src/main/java/nl/tudelft/oopp/group39/controllers/AdminBViewController.java
+++ client/src/main/java/nl/tudelft/oopp/group39/controllers/AdminBViewController.java
@@ -1,17 +1,9 @@
 package nl.tudelft.oopp.group39.controllers;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -23,12 +15,13 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import nl.tudelft.oopp.group39.communication.ServerCommunication;
 import nl.tudelft.oopp.group39.models.Building;
 
 
 
 
-public class AdminBViewController extends BuildingModifierController implements Initializable {
+public class AdminBViewController extends MainSceneController implements Initializable {
 
     @FXML
     private Button backbtn;
@@ -41,8 +34,6 @@ public class AdminBViewController extends BuildingModifierController implements 
     @FXML
     private TextField nameFieldNew;
     @FXML
-    private TextField updateBuildingField;
-    @FXML
     private TextField locationFieldNew;
     @FXML
     private TextField descriptionFieldNew;
@@ -51,7 +42,11 @@ public class AdminBViewController extends BuildingModifierController implements 
     @FXML
     private TextField timeClosedFieldNew;
     @FXML
+    private TextField updateBuildingField;
+    @FXML
     private TableColumn<Building, String> buildingnameCol = new TableColumn<>("Name");
+    @FXML
+    private TableColumn<Building, String> buildingidCol = new TableColumn<>("ID");
     @FXML
     private TableColumn<Building, String> buildinglocationCol = new TableColumn<>("Address");
     @FXML
@@ -59,36 +54,47 @@ public class AdminBViewController extends BuildingModifierController implements 
     @FXML
     private TableColumn<Building, LocalTime> buildingOpenTimeCol = new TableColumn<>("Open Time");
     @FXML
-    private TableColumn<Building, LocalTime> buildingClTimeCol = new TableColumn<>("Closing Time");
+    private TableColumn<Building, LocalTime> buildingCTimeCol = new TableColumn<>("Closing Time");
     @FXML
     private TableView<Building> buildingTable = new TableView<>();
 
 
+    /**
+     * Initialize data into tableView.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadBuildings();
+    }
 
     /**
-     * Display buildings and data in tableView buildingTable.
+     * Display buildings and data in tableView buildingTable. -- Likely doesn't work yet.
      */
     void loadBuildings() {
         buildingTable.setVisible(true);
         buildingTable.getItems().clear();
-        getBuildingsButton();
-
+        createAlert(ServerCommunication.get(ServerCommunication.building));
 
 
         buildingnameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        buildingidCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         buildinglocationCol.setCellValueFactory(new PropertyValueFactory<>("location"));
         buildingdescriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
         buildingOpenTimeCol.setCellValueFactory(new PropertyValueFactory<>("open"));
-        buildingClTimeCol.setCellValueFactory(new PropertyValueFactory<>("closed"));
+        buildingCTimeCol.setCellValueFactory(new PropertyValueFactory<>("closed"));
     }
 
 
-
     /**
-     * TODO - Add building using buttons.
+     * Adds a new building with auto-generated ID.
      */
     public void addBuilding() throws IOException {
-        newBuildingButton();
+        String name = nameFieldNew.getText();
+        String location = locationFieldNew.getText();
+        String desc = descriptionFieldNew.getText();
+        String open = getTime(timeOpenFieldNew.getText(), true);
+        String closed = getTime(timeClosedFieldNew.getText(), false);
+        createAlert(ServerCommunication.addBuilding(name, location, desc, open, closed));
 
         nameFieldNew.clear();
         locationFieldNew.clear();
@@ -99,11 +105,18 @@ public class AdminBViewController extends BuildingModifierController implements 
     }
 
     /**
-     * TODO - Update building using buttons.
+     * Updates building based on ID specified.
      */
 
     public void updateBuilding() throws IOException {
-        updateBuildingButton();
+        String name = nameFieldNew.getText();
+        String location = locationFieldNew.getText();
+        String desc = descriptionFieldNew.getText();
+        String open = getTime(timeOpenFieldNew.getText(), true);
+        String closed = getTime(timeClosedFieldNew.getText(), false);
+        String id = updateBuildingField.getText();
+        id = id.contentEquals("") ? "1" : id;
+        createAlert(ServerCommunication.updateBuilding(name, location, desc, open, closed, id));
 
         nameFieldNew.clear();
         locationFieldNew.clear();
@@ -117,9 +130,22 @@ public class AdminBViewController extends BuildingModifierController implements 
      * Delete building.
      */
     public void deleteBuilding() throws IOException {
-        removeBuildingButton();
+        String id = updateBuildingField.getText();
+        id = id.contentEquals("") ? "1" : id;
+        ServerCommunication.removeBuilding(id);
+        createAlert(ServerCommunication.get(ServerCommunication.building));
 
         updateBuildingField.clear();
+    }
+
+    /**
+     * Doc. TODO
+     */
+    public String getTime(String time, boolean open) {
+        if (open) {
+            return time.contentEquals("") ? LocalTime.MAX.toString() : time;
+        }
+        return time.contentEquals("") ? LocalTime.MIN.toString() : time;
     }
 
     /**
@@ -133,8 +159,4 @@ public class AdminBViewController extends BuildingModifierController implements 
         currentstage.setScene(new Scene(root, 700, 600));
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
 }
--- client/src/main/java/nl/tudelft/oopp/group39/controllers/AdminRoomViewController.java
+++ client/src/main/java/nl/tudelft/oopp/group39/controllers/AdminRoomViewController.java
@@ -1,29 +1,126 @@
 package nl.tudelft.oopp.group39.controllers;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import nl.tudelft.oopp.group39.communication.ServerCommunication;
+import nl.tudelft.oopp.group39.models.Room;
 
-public class AdminRoomViewController {
+public class AdminRoomViewController extends MainSceneController implements Initializable {
     @FXML
     private Button backbtn;
+    @FXML
+    private TextField updateRoomField;
+    @FXML
+    private TextField roomFacilitiesField;
+    @FXML
+    private TextField roomDescriptionField;
+    @FXML
+    private TextField roomBuildingIdField;
+    @FXML
+    private TextField roomOnlyStaffField;
+    @FXML
+    private TextField roomCapacityField;
+    @FXML
+    private TableView<Room> roomTable = new TableView<>();
+    @FXML
+    private TableColumn<Room, String> roomIdCol = new TableColumn<>("ID");
+    @FXML
+    private TableColumn<Room, String> roomBuildingIdCol = new TableColumn<>("Building ID");
+    @FXML
+    private TableColumn<Room, String> roomCapacityCol = new TableColumn<>("Capacity");
+    @FXML
+    private TableColumn<Room, String> roomFacilityCol = new TableColumn<>("Facilities");
+    @FXML
+    private TableColumn<Room, String> roomStaffCol = new TableColumn<>("Only staff");
+    @FXML
+    private TableColumn<Room, String> roomNameCol = new TableColumn<>("Name");
+
+
+    /**
+     * Initialize data into tableView.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadBuildings();
+    }
+
+    /**
+     * Display rooms and data in tableView buildingTable. -- Likely doesn't work yet. Need to add nameCol.
+     */
+    void loadBuildings() {
+        roomTable.setVisible(true);
+        roomTable.getItems().clear();
+        createAlert(ServerCommunication.get(ServerCommunication.building));
+
+
+        roomBuildingIdCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        roomIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        roomCapacityCol.setCellValueFactory(new PropertyValueFactory<>("location"));
+        roomFacilityCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        roomStaffCol.setCellValueFactory(new PropertyValueFactory<>("open"));
+        roomNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+    }
+
+
+    /**
+     * Adds a new Room with auto-generated ID.
+     */
+    public void addRoom() throws IOException {
+        String buildingId = roomBuildingIdField.getText();
+        String roomCapacity = roomCapacityField.getText();
+        String roomDescription = roomDescriptionField.getText();
+        createAlert(ServerCommunication.addRoom(buildingId, roomCapacity, roomDescription));
+
+    }
+
+    /**
+     * Updates room based on ID specified.
+     */
+
+    public void updateRoom() throws IOException {
+        String buildingId = roomBuildingIdField.getText();
+        String roomCap = roomCapacityField.getText();
+        String roomDesc = roomDescriptionField.getText();
+        String roomID = updateRoomField.getText();
+        roomID = roomID.contentEquals("") ? "1" : roomID;
+
+        createAlert(ServerCommunication.updateRoom(buildingId, roomCap, roomDesc, roomID));
+    }
+
+    /**
+     * Delete room.
+     */
+    public void deleteRoom() throws IOException {
+        String id = updateRoomField.getText();
+        id = id.contentEquals("") ? "1" : id;
+        ServerCommunication.removeRoom(id);
+
+        createAlert(ServerCommunication.get(ServerCommunication.room));
+
+    }
 
 
     /**
      * Goes back to main admin panel.
      */
+
     @FXML
     private void switchBack() throws IOException {
         Stage currentstage = (Stage) backbtn.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/AdminPanel.fxml"));
         currentstage.setScene(new Scene(root, 700, 600));
     }
-
-
 }
 
