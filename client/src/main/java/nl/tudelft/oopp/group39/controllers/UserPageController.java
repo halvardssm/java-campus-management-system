72fa298f925850cd117c42a0c0e0523dfa0d38c5
Patrick Hibbs <p.j.hibbs@student.tudelft.nl>
2020-03-30 15:22:47 +0200

    You can't have 2 bookings at the same date and starting time and you can book rooms from 9 until 8

--- client/src/main/java/nl/tudelft/oopp/group39/controllers/UserPageController.java
+++ client/src/main/java/nl/tudelft/oopp/group39/controllers/UserPageController.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.AnchorPane;
 import nl.tudelft.oopp.group39.communication.ServerCommunication;
 import java.io.IOException;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.FlowPane;
@@ -129,6 +131,16 @@ public class UserPageController extends MainSceneController {
         editDuration.setOpacity(1);
         editDate.setOpacity(1);
         doneButton.setOpacity(1);
+
+
+        String inputTimeString = "10:83:00";
+        try {
+            LocalTime.parse(inputTimeString);
+            System.out.println("Valid time string");
+        } catch (DateTimeParseException | NullPointerException e) {
+            System.out.println("Invalid Time String: " + inputTimeString);
+        }
+
     }
 
     public void finishEditBooking() {
@@ -137,13 +149,53 @@ public class UserPageController extends MainSceneController {
         editDate.setOpacity(0);
         doneButton.setOpacity(0);
 
+        try {
+            LocalTime sTime = LocalTime.parse(editStartingTime.getText());
+            LocalTime dTime = LocalTime.parse(editDuration.getText());
+
+            if(sTime.getHour() < 9) {
+                createAlert("You can only book a room after 9");
+                return;
+            }
+            if(dTime.getHour() > 4) {
+                createAlert("You can't book a room longer than 4 hours");
+                return;
+            }
+            if(sTime.getHour() == 17 && dTime.getHour() == 4 ||
+               sTime.getHour() == 18 && dTime.getHour() == 3 ||
+               sTime.getHour() == 19 && dTime.getHour() == 2 ||
+               sTime.getHour() == 20) {
+                createAlert("You can only book a room until 8");
+                return;
+            }
+
+            //Looking whether the user already has a booking at the same time and date
+            String bookingString = ServerCommunication.getAllBookings();
+            ArrayNode body = (ArrayNode) mapper.readTree(bookingString).get("body");
+            bookingString = mapper.writeValueAsString(body);
+            BookingDTO[] bookingList = mapper.readValue(bookingString, BookingDTO[].class);
+
+            for(BookingDTO booking : bookingList) {
+                if(booking.getStartTime().equals(editStartingTime.getText()) &&
+                editDate.getValue().toString().equals(booking.getDate())) {
+                    createAlert("You can't have 2 bookings at the same time!");
+                    return;
+                }
+            }
+
+        } catch (DateTimeParseException | NullPointerException e) {
+            System.out.println("Invalid Time String");
+        } catch (IOException e) {
+            System.out.println("Error: Wrong IO");
+        }
+
+
         LocalTime t1 = LocalTime.parse(editStartingTime.getText());
         LocalTime t2 = LocalTime.parse(editDuration.getText());
         int endTimeHour = t1.getHour() + t2.getHour();
 
         ServerCommunication.addBooking(editDate.getValue().toString(), t1.toString() + ":00", endTimeHour + ":00:00", user.getUsername(), roomID.getText());
         ServerCommunication.removeBooking(bookingID.getText());
-        //showBookings();
     }
 
     public void viewRoom() throws IOException {
