e41e31df7444c3d982835e312d38c14a23e8bf4b
Patrick Hibbs <p.j.hibbs@student.tudelft.nl>
2020-03-30 19:45:29 +0200

    Fixed more checkstyle

--- client/src/main/java/nl/tudelft/oopp/group39/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/group39/communication/ServerCommunication.java
@@ -113,19 +113,6 @@ public class ServerCommunication {
     }
 
     /**
-     * Retrieves bookings from the server.
-     *
-     * @return the body of a get request to the server.
-     */
-    public static String getBookings() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url + "booking"))
-                .build();
-        return httpRequest(request);
-    }
-
-    /**
      * Retrieves filtered list of buildings from the server.
      *
      * @return the body of a get request to the server.
