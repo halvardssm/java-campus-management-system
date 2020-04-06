f78d647185ef623bebe27951f9a605c523f9845d
Alexandru Toader <A.Toader@student.tudelft.nl>
2020-04-06 02:22:42 +0200

    Accidentally made main app use AdminPanel instead of UsersDisplay change

--- client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/group39/MainApp.java
@@ -1,10 +1,9 @@
 package nl.tudelft.oopp.group39;
 
 import nl.tudelft.oopp.group39.server.views.UsersDisplay;
-import nl.tudelft.oopp.group39.views.AdminPanel;
 
 public class MainApp {
     public static void main(String[] args) {
-        AdminPanel.main(new String[0]);
+        UsersDisplay.main(new String[0]);
     }
 }
